1976c144af0e490973daa6c750ab5e38ca942423
htlsne <htlsne23@gmail.com>
2021-12-28 15:18:10 +0900

    Implement tabs sample

--- popup/Popup.tsx
+++ popup/Popup.tsx
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as browser from 'webextension-polyfill';
 
-export const Popup: React.VFC = () => <div>Hello</div>;
+export const Popup: React.VFC = () => {
+
+  const [titles, setTitles] = useState<string[] | undefined>(undefined);
+
+  useEffect(() => {
+    (
+      async () => {
+        setTitles((await browser.tabs.query({})).map(tab => tab.title!));
+      })();
+  }, []);
+
+  if (titles == null) {
+    return <div>Loading...</div>;
+  }
+
+  return (<ul>{titles.map(title => <li key={title}>{title}</li>)}</ul>);
+}
